9eda69ee38cb0fa44dea6e79e005ffbcdb189f13
Omkarsalunkhe09 <ossalunkhe09@gmail.com>
2022-04-02 17:20:47 +0530

    updates

--- express-intro/src/configs/db.js
+++ express-intro/src/configs/db.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
-// step 1 :- connect to mongodb
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://ok1234:ok1234@cluster0.hvjws.mongodb.net/bckend?retryWrites=true&w=majority"
+    "mongodb+srv://ok1234:ok1234@cluster0.hvjws.mongodb.net/pops?retryWrites=true&w=majority"
   );
 };
 
